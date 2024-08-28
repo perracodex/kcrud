8caadd4637e06138271a00b3a739fa7033d79ba5
Fernando Sanchez <perracolabs@gmail.com>
2024-08-29 00:59:26 +0200

    Fixed linters

--- kcrud-base/src/main/kotlin/kcrud/base/database/plugin/DbPlugin.kt
+++ kcrud-base/src/main/kotlin/kcrud/base/database/plugin/DbPlugin.kt
@@ -11,17 +11,6 @@ import kcrud.base.settings.AppSettings
 import org.jetbrains.exposed.sql.Table
 
 /**
- * Configuration for the [DbPlugin].
- */
-internal class DbPluginConfig {
-    /** List of tables to be registered with the database. */
-    val tables: MutableList<Table> = mutableListOf()
-
-    /** Optional [PrometheusMeterRegistry] instance for micro-metrics monitoring. */
-    var micrometerRegistry: PrometheusMeterRegistry? = null
-}
-
-/**
  * Custom Ktor plugin to configure the database.
  */
 internal val DbPlugin: ApplicationPlugin<DbPluginConfig> = createApplicationPlugin(
@@ -36,4 +25,15 @@ internal val DbPlugin: ApplicationPlugin<DbPluginConfig> = createApplicationPlug
             addTable(table)
         }
     }
+}
+
+/**
+ * Configuration for the [DbPlugin].
+ */
+internal class DbPluginConfig {
+    /** List of tables to be registered with the database. */
+    val tables: MutableList<Table> = mutableListOf()
+
+    /** Optional [PrometheusMeterRegistry] instance for micro-metrics monitoring. */
+    var micrometerRegistry: PrometheusMeterRegistry? = null
 }
