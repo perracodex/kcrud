8254c8ef3e8ed6065454e86d6d512af3c5ee6534
Fernando Sanchez <perracolabs@gmail.com>
2024-09-05 18:06:28 +0200

    Updated AppSettings documentation

--- kcrud-base/src/main/kotlin/kcrud/base/settings/AppSettings.kt
+++ kcrud-base/src/main/kotlin/kcrud/base/settings/AppSettings.kt
@@ -6,6 +6,7 @@ package kcrud.base.settings
 
 import io.ktor.server.config.*
 import kcrud.base.env.Tracer
+import kcrud.base.settings.AppSettings.load
 import kcrud.base.settings.annotation.ConfigurationAPI
 import kcrud.base.settings.config.ConfigurationCatalog
 import kcrud.base.settings.config.parser.ConfigClassMap
@@ -19,37 +20,89 @@ import kotlinx.serialization.json.Json
 import kotlin.system.measureTimeMillis
 
 /**
- * Singleton providing the configuration settings throughout the application.
+ * Singleton object that serves as the central manager for application configuration settings.
  *
- * This class serves as the central point for accessing all configuration settings in a type-safe manner.
+ * AppSettings is responsible for loading, accessing, and managing all configuration settings in a type-safe manner.
+ * It provides properties that correspond to different aspects of the application's configuration, such as API, CORS,
+ * database, and more. Each property accesses a specific segment of the loaded configuration settings ensuring that
+ * all configuration data is available through a single access point.
+ *
+ * It is critical to invoke the [load] method as the first operation during the application's startup to ensure
+ * all other components have consistent access to the configuration settings.
+ * The [load] method uses [runBlocking] to synchronize configuration loading, making it safe for one-time initialization.
  */
 public object AppSettings {
+    private val tracer = Tracer<AppSettings>()
+
+    /**
+     * Holds the instance of [ConfigurationCatalog] containing all configuration settings for the application.
+     * This property is lazily initialized and volatile to ensure thread-safe access and correct initialization
+     * before its use across different parts of the application.
+     *
+     * The [configuration] is loaded once through the [load] method and accessed via the properties of [AppSettings].
+     * It is critical that this property is initialized at the start of the application to ensure all configuration
+     * settings are available and consistent throughout the application's lifecycle.
+     */
     @Volatile
     private lateinit var configuration: ConfigurationCatalog
 
-    /** The API schema settings. */
+    /**
+     * Provides access to the API schema configuration settings.
+     * These settings manage how API documentation is served and under what conditions it is available.
+     * Includes paths and settings for Swagger UI, Redoc UI, and OpenAPI specifications, allowing
+     * documentation to be tailored and accessible based on the specified environments.
+     */
     public val apiSchema: ApiSchemaSettings get() = configuration.apiSchema
 
-    /** The CORS settings. */
+    /**
+     * Provides access to the Cross-Origin Resource Sharing (CORS) settings.
+     * These settings manage the allowed origins for cross-origin requests, specifying which domains,
+     * schemes, and subdomains can interact with the application. This configuration can include detailed
+     * rules per host and can be set to universally allow all hosts under certain conditions.
+     */
     public val cors: CorsSettings get() = configuration.cors
 
-    /** The database settings. */
+    /**
+     * Provides access to the database configuration settings.
+     * These settings manage critical parameters for database operations, including the connection details,
+     * transaction management, query performance monitoring, and schema updates. This includes settings for
+     * connection pooling, optional authentication details, etc.
+     */
     public val database: DatabaseSettings get() = configuration.database
 
-    /** The deployment settings. */
+    /**
+     * Provides access to the deployment configuration settings.
+     * These settings define the network parameters for the server, including the ports for HTTP and HTTPS
+     * connections and the host address. This configuration is crucial for defining how and where the server
+     * listens for incoming connections.
+     */
     public val deployment: DeploymentSettings get() = configuration.deployment
 
-    /** The runtime settings. */
+    /**
+     * Provides access to the runtime configuration settings.
+     * These settings determine critical runtime parameters, such as the server's machine identification for
+     * traceability, the operational environment settings, and specific runtime behaviors like file storage locations.
+     */
     public val runtime: RuntimeSettings get() = configuration.runtime
 
-    /** The application security settings. */
+    /**
+     * Provides access to the security configuration settings.
+     * These settings encompass various security protocols and features, including authentication methods (Basic, JWT, OAuth),
+     * encryption standards, and security constraints like rate limiting. This configuration is essential for ensuring the
+     * application's security posture across all interactions.
+     */
     public val security: SecuritySettings get() = configuration.security
 
     /**
-     * Loads the application settings from the provided [ApplicationConfig].
-     * Will only load the settings once.
+     * Initializes the application's configuration settings by loading them from the provided [ApplicationConfig].
+     * This method must be called as the very first step during the application startup process to ensure that all
+     * other components have access to a consistently configured environment.
      *
-     * @param applicationConfig The [ApplicationConfig] to load the settings from.
+     * The settings are loaded exactly only once. This method employs [runBlocking] to ensure that all configuration
+     * settings are loaded synchronously before proceeding with any further application initialization.
+     * The use of [runBlocking] is appropriate here due to its one-time execution at startup.
+     *
+     * @param applicationConfig The [ApplicationConfig] instance from which the settings are loaded.
      */
     @OptIn(ConfigurationAPI::class)
     public fun load(applicationConfig: ApplicationConfig) {
@@ -57,14 +110,14 @@ public object AppSettings {
             return
         }
 
-        val tracer = Tracer(ref = ::load)
         tracer.info("Loading application settings.")
 
-        val timeTaken = measureTimeMillis {
-            // List of configuration mappings. Each entry maps a section in the configuration file
-            // to a data class that will hold the configuration values. The 'mappingName' is used as a key
-            // for the mapping, 'path' specifies the location in the configuration file, and 'kClass' refers
-            // to the Kotlin class used for data instantiation.
+        val timeTaken: Long = measureTimeMillis {
+            // List defining the mappings between configuration file sections and properties within ConfigurationCatalog.
+            // Each entry in the list consists of three components:
+            // 1. 'mappingName' - The name of the property within ConfigurationCatalog to hold the loaded configuration data.
+            // 2. 'path' - The location in the configuration file (e.g., "ktor.deployment") from where the settings are loaded.
+            // 3. 'kClass' - The data class type used to instantiate and hold the configuration data for that section.
             val configMappings: List<ConfigClassMap<out IConfigSection>> = listOf(
                 ConfigClassMap(mappingName = "apiSchema", path = "apiSchema", kClass = ApiSchemaSettings::class),
                 ConfigClassMap(mappingName = "cors", path = "cors", kClass = CorsSettings::class),
@@ -89,9 +142,10 @@ public object AppSettings {
     }
 
     /**
-     * Serializes the current settings configuration to a JSON string.
-     * The [load] method must have been called at least once before
-     * ever using this method, usually during application startup.
+     * Serializes the current settings configuration into a JSON string.
+     * This method is intended for use in scenarios where the application's settings need to be exported or logged.
+     * It is crucial that the [load] method has been successfully invoked at least once prior to using this method
+     * to ensure that the configuration data is fully initialized and consistent.
      *
      * @return The JSON string representation of the current settings.
      */
@@ -100,9 +154,15 @@ public object AppSettings {
     }
 
     /**
-     * Deserializes the provided JSON string into the current settings.
+     * Deserializes the provided JSON string into the application's current settings, effectively updating the
+     * configuration state. This method can be particularly useful for scenarios such as initializing [AppSettings]
+     * in a different class loader or environment where parsing the original configuration sources is not feasible.
      *
-     * @param string The JSON string to deserialize.
+     * It is crucial that the JSON string provided is properly formatted and structurally compatible with the
+     * [ConfigurationCatalog]. Failure to adhere to the expected structure can result in parsing errors,
+     * potentially leading to an inconsistent or erroneous application configuration state.
+     *
+     * @param string The JSON string to deserialize, which must accurately represent the structure of [ConfigurationCatalog].
      */
     public fun deserialize(string: String) {
         configuration = Json.decodeFromString<ConfigurationCatalog>(string = string)
