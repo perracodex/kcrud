1d3dd79bc2d7f576c2d94b58c4efa59a91e8eb8d
Fernando Sanchez <perracolabs@gmail.com>
2024-09-16 19:10:53 +0200

    Refactored Page class separating the details into a distinct class to improve OpenAPI generation

--- kcrud-base/src/main/kotlin/kcrud/base/persistence/pagination/Page.kt
+++ kcrud-base/src/main/kotlin/kcrud/base/persistence/pagination/Page.kt
@@ -4,39 +4,54 @@
 
 package kcrud.base.persistence.pagination
 
+import kcrud.base.persistence.pagination.Page.Details
 import kotlinx.serialization.Serializable
 
 /**
  * Holds the data for a page of results.
  *
- * @param totalPages The total number of pages available based on the pagination settings.
- * @param pageIndex The current page number, 0-based.
- * @param totalElements Total number of elements in the entire dataset, not just a page.
- * @param elementsPerPage The number of elements per each page.
- * @param elementsInPage The number of elements in the current page.
- * @param isFirst True if serving the first page.
- * @param isLast True if serving the last page.
- * @param hasNext True if there is a next page.
- * @param hasPrevious True if there is a previous page.
- * @param overflow Whether the requested page index is above the total available pages.
- * @param sort The optional sorting that has been applied to the content.
+ * @param details The pagination [Details] that describe the state of the page.
  * @param content The data that forms the content in a page.
  */
 @Serializable
 public data class Page<out T : Any>(
-    val totalPages: Int,
-    val pageIndex: Int,
-    val totalElements: Int,
-    val elementsPerPage: Int,
-    val elementsInPage: Int,
-    val isFirst: Boolean,
-    val isLast: Boolean,
-    val hasNext: Boolean,
-    val hasPrevious: Boolean,
-    val overflow: Boolean,
-    val sort: List<Pageable.Sort>?,
-    val content: List<T>
+    val details: Details,
+    val content: List<T>,
 ) {
+    /**
+     * The [Page] details that describe the pagination state.
+     *
+     * Set as a separate data class to simplify the OpenAPI documentation,
+     * so each generated Page specification per its concrete type does not
+     * have to include the entire Page details attributes.
+     *
+     * @param totalPages The total number of pages available based on the pagination settings.
+     * @param pageIndex The current page number, 0-based.
+     * @param totalElements Total number of elements in the entire dataset, not just a page.
+     * @param elementsPerPage The number of elements per each page.
+     * @param elementsInPage The number of elements in the current page.
+     * @param isFirst True if serving the first page.
+     * @param isLast True if serving the last page.
+     * @param hasNext True if there is a next page.
+     * @param hasPrevious True if there is a previous page.
+     * @param overflow Whether the requested page index is above the total available pages.
+     * @param sort The optional sorting that has been applied to the content.
+     */
+    @Serializable
+    public data class Details(
+        val totalPages: Int,
+        val pageIndex: Int,
+        val totalElements: Int,
+        val elementsPerPage: Int,
+        val elementsInPage: Int,
+        val isFirst: Boolean,
+        val isLast: Boolean,
+        val hasNext: Boolean,
+        val hasPrevious: Boolean,
+        val overflow: Boolean,
+        val sort: List<Pageable.Sort>?,
+    )
+
     public companion object {
         /**
          * Factory method to create a new [Page] instance.
@@ -70,17 +85,19 @@ public data class Page<out T : Any>(
 
             // Construct the Page object with the determined states.
             return Page(
-                totalPages = totalPages,
-                pageIndex = pageIndex,
-                totalElements = totalElements,
-                elementsPerPage = pageSize,
-                elementsInPage = elementsInPage,
-                isFirst = isFirst,
-                isLast = isLast,
-                hasNext = hasNext,
-                hasPrevious = hasPrevious,
-                overflow = overflow,
-                sort = pageable?.sort,
+                details = Details(
+                    totalPages = totalPages,
+                    pageIndex = pageIndex,
+                    totalElements = totalElements,
+                    elementsPerPage = pageSize,
+                    elementsInPage = elementsInPage,
+                    isFirst = isFirst,
+                    isLast = isLast,
+                    hasNext = hasNext,
+                    hasPrevious = hasPrevious,
+                    overflow = overflow,
+                    sort = pageable?.sort
+                ),
                 content = content
             )
         }
