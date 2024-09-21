36600928823bd96d1052f72c61bdc4922d80f5d9
Fernando Sanchez <perracolabs@gmail.com>
2024-09-21 09:11:32 +0200

    Removed unused CIO dependency

--- kcrud-access/build.gradle.kts
+++ kcrud-access/build.gradle.kts
@@ -9,7 +9,6 @@ dependencies {
 
     implementation(project(":kcrud-base"))
 
-    implementation(libs.ktor.client.cio)
     implementation(libs.ktor.serialization.kotlinx.json)
     implementation(libs.ktor.server.auth)
     implementation(libs.ktor.server.auth.jwt)
