c4a1e8a67d1bcca429307bb9e1dad792625e4ce6
Fernando Sanchez <perracolabs@gmail.com>
2024-10-05 21:27:02 +0200

    Cleanup

--- kcrud-core/src/main/kotlin/kcrud/core/env/health/HealthCheck.kt
+++ kcrud-core/src/main/kotlin/kcrud/core/env/health/HealthCheck.kt
@@ -26,7 +26,6 @@ import kotlinx.serialization.Serializable
  */
 @OptIn(HealthCheckAPI::class)
 @Serializable
-@ConsistentCopyVisibility
 public data class HealthCheck internal constructor(
     val health: MutableList<String>,
     val application: ApplicationHealth,
