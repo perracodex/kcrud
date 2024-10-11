79c404bbe170019598f9acac871a7799e518230e
Fernando Sanchez <perracolabs@gmail.com>
2024-10-11 18:27:51 +0200

    Refactored Route collector

--- kcrud-core/src/main/kotlin/kcrud/core/utils/RouteCollector.kt
+++ kcrud-core/src/main/kotlin/kcrud/core/utils/RouteCollector.kt
@@ -10,6 +10,15 @@ import kcrud.core.env.health.annotation.HealthCheckAPI
 import kotlinx.serialization.Serializable
 
 /**
+ * Holds information about a registered route.
+ *
+ * @property path The path of the route.
+ * @property method The HTTP method of the route.
+ */
+@Serializable
+public data class RouteInfo(val path: String, val method: String)
+
+/**
  * Extension function traverse and collect all routes.
  *
  * @return A list of all found routes.
@@ -18,27 +27,19 @@ import kotlinx.serialization.Serializable
 internal fun Application.collectRoutes(): List<RouteInfo> {
     val routes: MutableList<RouteInfo> = mutableListOf()
 
-    // Helper function to recursively traverse routes and collect attribute values.
-    fun Route.collectAttributes() {
-        when (this) {
-            is RoutingRoot -> {
-                this.children.forEach { it.collectAttributes() }
-            }
-
-            is RoutingNode -> {
-                if (this.selector is HttpMethodRouteSelector) {
-                    val method: String = (this.selector as HttpMethodRouteSelector).method.value
-                    val path: String = this.extractEndpointPath()
-                    val routeInfo = RouteInfo(path = path, method = method)
-                    routes.add(routeInfo)
-                }
-                this.children.forEach { it.collectAttributes() }
-            }
+    // Helper function to recursively traverse and collect routes.
+    fun RoutingNode.collect() {
+        if (this.selector is HttpMethodRouteSelector) {
+            val method: String = (this.selector as HttpMethodRouteSelector).method.value
+            val path: String = this.extractRoutePath()
+            val routeInfo = RouteInfo(path = path, method = method)
+            routes.add(routeInfo)
         }
+        this.children.forEach { it.collect() }
     }
 
     // Start collecting from the root route.
-    this.routing { }.collectAttributes()
+    this.routing { }.children.forEach { it.collect() }
 
     return routes.sortedWith(
         compareBy(
@@ -47,15 +48,6 @@ internal fun Application.collectRoutes(): List<RouteInfo> {
         )
     )
 }
-
-/**
- * Holds information about a registered route.
- *
- * @property path The path of the route.
- * @property method The HTTP method of the route.
- */
-@Serializable
-public data class RouteInfo(val path: String, val method: String)
 
 /**
  * Constructs the full path of a route by aggregating path segments from the current route up to the root,
@@ -69,32 +61,30 @@ public data class RouteInfo(val path: String, val method: String)
  * @return A string representing the full path from the root to the current route, starting with a `/`.
  * If the current route is at the root or no path segments are defined, the function returns just `/`.
  */
-internal fun Route.extractEndpointPath(): String {
+private fun RoutingNode.extractRoutePath(): String {
     val segments: MutableList<String> = mutableListOf()
     var currentRoute: Route? = this
 
     // Traverse the parent chain of the current route and collect path segments.
-    while (currentRoute != null) {
-        val selector: RouteSelector = when (this) {
-            is RoutingRoot -> this.selector
-            is RoutingNode -> this.selector
-            else -> break
-        }
+    while (currentRoute is RoutingNode) {
+        val selector: RouteSelector = currentRoute.selector
 
-        val segment: String = when (selector) {
+        when (selector) {
             is PathSegmentConstantRouteSelector -> selector.value
             is PathSegmentParameterRouteSelector -> "{${selector.name}}"
             is PathSegmentOptionalParameterRouteSelector -> "{${selector.name}?}"
             is PathSegmentWildcardRouteSelector -> "*"
-            is TrailingSlashRouteSelector -> ""
-            is HttpMethodRouteSelector -> "" // Skip HTTP method selectors
+            is TrailingSlashRouteSelector -> ""  // Ignored for path construction
+            is HttpMethodRouteSelector -> ""     // Ignored for path construction
             else -> ""
+        }.let { segment ->
+            if (segment.isNotEmpty()) {
+                segments.add(segment)
+            }
         }
-        if (segment.isNotEmpty()) {
-            segments.add(segment)
-        }
+
         currentRoute = currentRoute.parent
     }
 
-    return segments.reversed().joinToString(separator = "/", prefix = "/").trim()
+    return segments.asReversed().joinToString(separator = "/", prefix = "/").trimEnd('/')
 }
