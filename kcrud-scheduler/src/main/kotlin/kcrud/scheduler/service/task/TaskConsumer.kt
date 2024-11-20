3bd09b08ae1f64ec57817fbea96e3b177b49b5c3
Fernando Sanchez <perracolabs@gmail.com>
2024-11-20 21:47:00 +0100

    Added re-scheduling option

--- kcrud-scheduler/src/main/kotlin/kcrud/scheduler/service/task/TaskConsumer.kt
+++ kcrud-scheduler/src/main/kotlin/kcrud/scheduler/service/task/TaskConsumer.kt
@@ -7,6 +7,7 @@ package kcrud.scheduler.service.task
 import kcrud.core.event.SseService
 import kcrud.core.util.DateTimeUtils.current
 import kcrud.core.util.DateTimeUtils.formatted
+import kcrud.scheduler.service.schedule.TaskStartAt
 import kcrud.scheduler.service.task.TaskConsumer.Payload
 import kotlinx.datetime.LocalDateTime
 import org.quartz.Job
@@ -23,6 +24,9 @@ import kotlin.uuid.Uuid
  */
 public abstract class TaskConsumer<P : Payload> : Job {
 
+    /** The re-schedule time for the task. */
+    private var rescheduleAt: TaskStartAt? = null
+
     /**
      * Initiates the task execution.
      *
@@ -36,7 +40,19 @@ public abstract class TaskConsumer<P : Payload> : Job {
             .filterKeys { it is String }
             .mapKeys { it.key as String }
 
-        start(properties = properties)
+        try {
+            start(properties = properties)
+        } finally {
+            // Re-schedule the job if requested to do so.
+            rescheduleAt?.let { startAt ->
+                TaskReScheduler(
+                    scheduler = context.scheduler,
+                    jobDetail = context.jobDetail,
+                    startAt = startAt
+                ).reschedule()
+                rescheduleAt = null
+            }
+        }
     }
 
     /**
@@ -57,6 +73,7 @@ public abstract class TaskConsumer<P : Payload> : Job {
                         "| Task Id: ${payload.taskId} " +
                         "| Error: ${error.message.orEmpty()}"
             )
+
             // Rethrow the exception to allow it to propagate.
             throw error
         }.onSuccess {
@@ -67,6 +84,16 @@ public abstract class TaskConsumer<P : Payload> : Job {
                         "| Task Id: ${payload.taskId} "
             )
         }
+    }
+
+    /**
+     * Set the re-schedule of the task for a future time, for example, in case of a failure.
+     *
+     * #### Attention
+     * This affects only one-time tasks. Recurring tasks are left unaffected to prevent multiple triggers.
+     */
+    protected fun reschedule(startAt: TaskStartAt) {
+        rescheduleAt = startAt
     }
 
     /**
